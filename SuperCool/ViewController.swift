dc1040dacb7097061689a53b859ba46edb48e4c3
Dmitriy Loria <dima_loria@mail.ru>
2019-07-21 15:50:23 +0300

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Dima Loria on 21/07/2019.
+//  Copyright © 2019 Dima Loria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
